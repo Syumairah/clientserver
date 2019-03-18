e3d9e4389a378cfdc83832228fc080526762828b
Syumairah <48507635+Syumairah@users.noreply.github.com>
2019-03-18 22:39:18 +0800

    Create .c

---
+++ .c
@@ -0,0 +1,37 @@
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main()
+{
+  // CREATE A SOCKET
+  int network_socket;
+  network_socket = socket (AF_INET, SOCK_STREAM, 0);
+
+  // SPECIFY AN ADDRESS FOR THE SOCKET
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons (9302);
+  server_address.sin_address.s_addr = INADOR_ANY;
+
+  int connection_status = connect (network_socket, (struct sockaddr*) &server_address, sizeof (server_address));
+  
+  //CHECK FOR ERROR WITH CONNECTION
+  if (connection_status == -1)
+  {
+    printf ("There was an error making a connection to the remote socket \n\n");
+  }
+
+  //RECEIVE DATA FROM SERVER 
+  char server_response[256];
+  recv(network_socket, &server_response, sizeof(server_response, 0), 0);
+
+  // PRINT OUT THE SERVER'S RESPONSE
+  printf("The server sent the data : %s\n", server_response);
+
+  //END THE CLOSE THE SOCKET
+  close(network_sock);
+  return 0;
+}
