375e55483b86da076e23233af0f45d68c36bae7a
Syumairah <48507635+Syumairah@users.noreply.github.com>
2019-03-31 01:28:48 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,45 +1,35 @@
 #include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
 #include <netinet/in.h> 
-#include <string.h> 
 #include <unistd.h> 
 #include <arpa/inet.h>
-#define PORT 5132
    
-int main(int argc, char const *argv[]) 
+int main() 
 { 
-    struct sockaddr_in address; 
-    int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
-    char *hello = "Hello from server"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-   
     serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "192.168.127.130", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\n Address not supported \n"); 
-        return -1; 
-    } 
+    serv_addr.sin_port = htons(5132);
+    serv_addr.sin_addr.s_addr = ip address;
+    char serverMsg[1024] = "Hello yaww"; 
+    
+    int status = connect(network_socket,(struct sockaddr *) &server_address, sizeof(server_address));
    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection failed\n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Annyeonghaseyo, server! \n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
+   if (status == -1)
+   {
+      printf("There was an error %s\n");
+   }
+   
+   send(network_socket, serverMsg, sizeof(serverMsg), 0);
+   
+   char serverResponse [1024];
+   recv(network_socket, &serverResponse, sizeof(serverResponse), 0);
+   
+   printf("Annyeonghaseyo, server! : %s\n", serverResponse );
+   close (network_socket);
+   
+   return 0; 
 } 
