683ec85274d4cddd5d8f3238f5ba2eef646f5372
Syumairah <48507635+Syumairah@users.noreply.github.com>
2019-03-31 11:13:33 +0800

    Update client.c

--- client.c
+++ client.c
@@ -10,27 +10,30 @@
    
 int main() 
 { 
-    struct sockaddr_in serv_addr; 
+    int networkSocket;
+    networkSocket = socket(AF_INET, SOCK_STREAM, 0);
+   
+    struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(5132);
     serv_addr.sin_addr.s_addr = ip address;
    
     char serverMsg[1024] = "Hello yaww"; 
     
-    int status = connect(network_socket,(struct sockaddr *) &server_address, sizeof(server_address));
+    int status = connect(networkSocket,(struct sockaddr *) &server_address, sizeof(server_address));
    
    if (status == -1)
    {
       printf("There was an error %s\n");
    }
    
-   send(network_socket, serverMsg, sizeof(serverMsg), 0);
+   send(networkSocket, serverMsg, sizeof(serverMsg), 0);
    
    char serverResponse [1024];
-   recv(network_socket, &serverResponse, sizeof(serverResponse), 0);
+   recv(networkSocket, &serverResponse, sizeof(serverResponse), 0);
    
    printf("Annyeonghaseyo, server! : %s\n", serverResponse );
-   close (network_socket);
+   close (networkSocket);
    
    return 0; 
 } 
